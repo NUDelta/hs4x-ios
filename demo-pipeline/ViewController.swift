448a4cbb51366229cb6079fb82257a081a8bfefa
Delta Lab <elicohen2018@u.northwestern.edu>
2017-12-04 16:05:26 -0600

    comments

--- demo-pipeline/ViewController.swift
+++ demo-pipeline/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 import CoreLocation
 import AVFoundation
+import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    //init variables
     var locationManager: CLLocationManager! = CLLocationManager()
     let synthesizer : AVSpeechSynthesizer = AVSpeechSynthesizer()
     let serverAddress = "https://hs4x.herokuapp.com"
@@ -18,7 +20,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var momentPlayed = false
     var lastLocationPostedAt : Double = Date().timeIntervalSinceReferenceDate
     @IBOutlet weak var momentTextLabel: UILabel!
+    @IBOutlet var mapView: MKMapView!
     
+    //used to convert http string response to json
     func convertToDictionary(text: String) -> [String: Any] {
         if let data = text.data(using: .utf8) {
             do {
@@ -31,14 +35,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     // Begins tracking location after view did load
+    //called every time the location updates
     func locationManager(_:CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let userLocation:CLLocation = locations[0]
         let long = userLocation.coordinate.longitude;
         let lat = userLocation.coordinate.latitude;
+        
+        //code to make map follow user
+        let center = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.0005, longitudeDelta: 0.0005))
+        mapView.setRegion(region, animated: false)
+
         var json = [String:Double]()
         json["longitude"] = long
         json["latitude"] = lat
-        if Date().timeIntervalSinceReferenceDate - self.lastLocationPostedAt > 3 {
+        //dont post faster than once a second
+        if Date().timeIntervalSinceReferenceDate - self.lastLocationPostedAt > 1 {
             self.lastLocationPostedAt = Date().timeIntervalSinceReferenceDate
             print("lat: \(lat), long: \(long)")
             do {
@@ -53,6 +65,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 request.addValue("application/json", forHTTPHeaderField: "Content-Type")
                 request.addValue("application/json", forHTTPHeaderField: "Accept")
                 let task = URLSession.shared.dataTask(with: request) {
+                    //async callback - cant edit ui if not in main thread
                     (data: Data?, response: URLResponse?, error: Error?) in
                     if(error != nil) {
                         print("Error: \(error)")
@@ -62,9 +75,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                         print(outputStr!)
                         let momentDict = self.convertToDictionary(text: outputStr!)
                         if (momentDict["prompt"] != nil) {
+                            //set field
                             self.momentString = String(describing: momentDict["prompt"]!)
                             //print(self.momentString)
-                           // self.momentTextLabel.text = self.momentString
+                            //play the text
                             let utterance : AVSpeechUtterance = AVSpeechUtterance(string: self.momentString)
                             utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
                             self.synthesizer.speak(utterance)
@@ -72,7 +86,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                         else {
                             self.momentString = ""
                             // print(self.momentString)
-                            // self.momentTextLabel.text = self.momentString
                         }
                     }
                 }
@@ -82,10 +95,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             
         }
         
+        //since this is called regularly, it will update the ui one call after the field has been set
+        if self.momentString != "" {
+            self.momentTextLabel.text = self.momentString
+        }
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //location stuff:
+        
         // Ask for Authorisation from the User.
         self.locationManager.requestAlwaysAuthorization()
         
@@ -99,6 +120,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
         }
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.mapType = MKMapType(rawValue: 0)!
+        mapView.userTrackingMode = MKUserTrackingMode(rawValue: 1)!
     }
 
     override func didReceiveMemoryWarning() {
